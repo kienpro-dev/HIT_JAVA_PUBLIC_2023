3352f09fcc5d3250a0156c557e7163bbb4af4109
kienpro-dev <tienkiennropro@gmail.com>
2023-09-01 13:11:09 +0700

    add BTVN b5

---
+++ Buoi5/Buoi5/src/BTVN/Bai2/Main.java
@@ -0,0 +1,29 @@
+package BTVN.Bai2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author TQDoanh
+ */
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Nhap so sinh vien:");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        Student[] students = new Student[n];
+
+        for(int i=0; i<n; i++){
+            students[i] = new Student();
+            students[i].Input();
+        }
+
+        System.out.printf("%-20s%-10s%-10s%-20s%-15s%-20s%-15s\n", "Ten SV", "Lop", "Diem so", "Ten khoa", "Ngay vao", "Ten truong", "Ngay vao");
+
+        for(int i=0; i<n; i++){
+            students[i].Output();
+        }
+    }
+}
---
+++ Buoi6/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Student a = new Student("Kien", 10, 1.0);
+        a.display();
+    }
+}
